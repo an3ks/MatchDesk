79ff4320ef5cf30e9e45865ae07a3245c1d5d7e4
an3ks <avdienkodanya99@gmail.com>
2025-03-03 21:34:40 +0300

    Initial Commit

---
+++ MatchDesk/MatchDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchDeskApp.swift
+//  MatchDesk
+//
+//  Created by Данила Авдиенко on 03.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MatchDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MatchDeskTests/MatchDeskTests.swift
@@ -0,0 +1,17 @@
+//
+//  MatchDeskTests.swift
+//  MatchDeskTests
+//
+//  Created by Данила Авдиенко on 03.03.2025.
+//
+
+import Testing
+@testable import MatchDesk
+
+struct MatchDeskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
