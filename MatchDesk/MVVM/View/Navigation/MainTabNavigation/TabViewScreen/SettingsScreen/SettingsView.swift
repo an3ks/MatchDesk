daf2b44f94d145d250908197e74148de07caa880
an3ks <avdienkodanya99@gmail.com>
2025-04-26 12:16:47 +0300

    Settings view done + AppStorage for color theme

---
+++ MatchDesk/MVVM/View/Navigation/MainTabNavigation/TabViewScreen/SettingsScreen/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  MatchDesk
+//
+//  Created by Данила Авдиенко on 25.04.2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
