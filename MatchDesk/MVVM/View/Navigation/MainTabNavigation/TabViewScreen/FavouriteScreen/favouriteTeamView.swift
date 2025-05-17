2715c62cc16fc4e916e1151b61694d2a4d6126af
an3ks <avdienkodanya99@gmail.com>
2025-05-17 14:03:00 +0300

    Created favourite team view + set data passing for proper work

---
+++ MatchDesk/MVVM/View/Navigation/MainTabNavigation/TabViewScreen/FavouriteScreen/favouriteTeamView.swift
@@ -0,0 +1,18 @@
+//
+//  favouriteTeamView.swift
+//  MatchDesk
+//
+//  Created by Данила Авдиенко on 16.05.2025.
+//
+
+import SwiftUI
+
+struct favouriteTeamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    favouriteTeamView()
+}
